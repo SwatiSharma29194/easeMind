92c81554827f8ec43c9014a30a91e389b8a2aca5
Swati Sharma <swati.sharma@advayafleet.com>
2026-01-20 16:13:49 -0700

    Initial Commit

---
+++ EaseMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EaseMind
+//
+//  Created by Rahul Sharma on 2026-01-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EaseMindTests/EaseMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  EaseMindTests.swift
+//  EaseMindTests
+//
+//  Created by Rahul Sharma on 2026-01-20.
+//
+
+import Testing
+@testable import EaseMind
+
+struct EaseMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
